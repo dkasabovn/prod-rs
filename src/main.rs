cc2ddad911b1f14ddcf41395983447a28b1d7de0
Akilan Manivannan <amanivannan@outlook.com>
2021-11-11 23:26:29 -0600

    Accepts letter as arg to specify mode
    CLI checks the first argument after filename and sees if it is a valid letter. If it's not, or if it is left blank, it displays a help message.
    
    Also stubbed functions for each mode.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 
 mod utils;
 
+use std::env;
+use std::collections::HashMap;
 
 use configparser::ini::Ini;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut config = Ini::new();
     let map = config.load("todo.conf")
         .expect("Unable to open file!");
@@ -15,5 +15,41 @@ fn main() {
     let list_ids = map.get("lists")
         .expect("No lists found!");
 
-    utils::pull(&list_ids);
+    
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(letter) => match letter.as_ref() {
+            "l" => list(&list_ids),                         //ordinary list
+            "u" => update(&list_ids),                       //update records
+            "i" => interactive(&list_ids),                  //interactive
+            _ => help("Invalid argument specified...")      //invalid option
+        },
+        None => help("No argument specified...")
+    }
+}
+
+fn help(error_message: &str) {
+    eprintln!("{}", error_message);
+
+    println!("Usage: todo <mode>");
+    println!("\nModes");
+    println!("l - List Mode");
+    println!("i - Interactive Mode");
+    println!("u - Update list with AWS S3");
+}
+
+fn list(list_ids: &HashMap<String, Option<String>>) {
+    println!("list")
+}
+
+fn update(list_ids: &HashMap<String, Option<String>>) {
+    println!("Updating list...");
+    match utils::pull(&list_ids) {
+        Ok(_) => println!("Update finished"),
+        Err(_) => eprintln!("Error fetching updates")
+    }
+}
+
+fn interactive(list_ids: &HashMap<String, Option<String>>) {
+    println!("interactive")
 }
